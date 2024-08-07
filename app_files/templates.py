7ad0e8ebd9b2b2fdb166b2a1494262ef83e96ff9
Cg-Prodigy <egomaritmbrian@gmail.com>
2024-08-07 22:57:57 +0300

    Manager boilerplate

--- app_files/templates.py
+++ app_files/templates.py
@@ -1,5 +1,8 @@
+from kivy.uix.screenmanager import ScreenManager
 from kivymd.uix.screen import MDScreen
 
 
+class Manager(ScreenManager):
+    pass
 class EntryPoint(MDScreen):
     pass
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from kivy.core.window import Window
 from kivy.factory import Factory
 from kaki.app import App
 from kivymd.app import MDApp
-Window.size=(360,640)
+
 class HotReload(App,MDApp):
-    CLASSES={'EntryPoint':'app_files.templates'}
+    CLASSES={'Manager':'app_files.templates'}
     KV_FILES=['kivy_files/kivy_lang.kv']
     AUTORELOADER_PATHS=[('app_files',{'recursive':True}),('kivy_files',{'recursive':True})]
     def build_app(self):
-        return Factory.EntryPoint()
+        return Factory.Manager()
 
 if __name__=='__main__':
     HotReload().run()
